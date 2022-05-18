d8e05918cf11f7e181676052362299ffdb1556ff
Kavyakrishnan <kavyakrishnan9821@gmail.com>
2022-05-18 20:49:16 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Addemp from './Component/Addemp';
+import Searchemp from './Component/Searchemp';
 
 function App() {
   return (
     <div>
       <Addemp/>
+      <Searchemp/>
     </div>
   );
 }
---
+++ src/Component/Searchemp.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Searchemp = () => {
+  return (
+    <div><div className="container">
+    <div className="row g-2">
+        <div className="col col-12 col sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Employeeid</label>
+                    <input type="text" class="form-control"/>
+                </div>
+                
+                
+                
+                
+                
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button class="btn btn-primary">Search</button>     
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div></div>
+  )
+}
+
+export default Searchemp
